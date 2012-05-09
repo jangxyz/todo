85e6e2750022278dd7925dcc76b4078acbba6709
Veselin Todorov <veselin@veselin.bg>
2012-05-09 23:31:14 +0300

    Introduce routes

---
+++ test/route.test.js
@@ -0,0 +1,63 @@
+/*!
+ * todo - Todos in the CLI like what.
+ *
+ * Veselin Todorov <hi@vesln.com>
+ * MIT License.
+ */
+
+/**
+ * Support.
+ */
+var should = require('chai').should();
+
+/**
+ * Subject.
+ */
+var Route = require('../lib/route');
+
+describe('Route', function() {
+  var handler = null;
+
+  before(function() {
+    handler = function handler() {};
+  });
+
+  describe('path', function() {
+    it('is converted to a regular expression', function() {
+      var route = new Route('foo', handler);
+      route.path().should.eql(/foo/);
+    });
+
+    it('can be fetched', function() {
+      var route = new Route(/foo/, handler);
+      route.path().should.eql(/foo/);
+    });
+
+    it('is required', function() {
+      (function() {
+        var route = new Route;
+      }).should.throw(Error);
+    });
+  });
+
+  describe('handler', function() {
+    it('can be fetched', function() {
+      var route = new Route(/foo/, handler);
+      route.handler().should.eql(handler);
+    });
+
+    it('is required', function() {
+      (function() {
+        var route = new Route(/path/);
+      }).should.throw(Error);
+    });
+  });
+
+
+  it('can tell if matches a given path', function() {
+    var route = new Route('foo', handler);
+
+    route.match(/foo/).should.be.true;
+    route.match(/bar/).should.be.false;
+  });
+});
